ef0e322490c5528676e3eb3bd5edf70c97e4347c
giankyle989 <kylelaguerta989@gmail.com>
2022-04-08 15:26:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/javamailapidemo/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.javamailapidemo;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText mEmail;
+    public EditText mSubject;
+    public EditText mMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mEmail = (EditText)findViewById(R.id.mailID);
+        mSubject = (EditText)findViewById(R.id.subjectID);
+        mMessage = (EditText)findViewById(R.id.messageID);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMail();
+            }
+        });
+    }
+
+    private void sendMail(){
+        String mail = mEmail.getText().toString().trim();
+        String subject = mSubject.getText().toString().trim();
+        String message = mMessage.getText().toString().trim();
+
+        JavaMailAPI javaMailAPI = new JavaMailAPI(this,mail,subject,message);
+
+        javaMailAPI.execute();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/javamailapidemo/Utils.java
@@ -0,0 +1,6 @@
+package com.example.javamailapidemo;
+
+public class Utils {
+    public static final String EMAIL ="kylelaguerta989@gmail.com";
+    public static final String PASSWORD ="wansbngwrkeucwgf";
+}
